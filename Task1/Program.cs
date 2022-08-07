126ca365bec28b7665eb0b9050380f018bc49b35
SilverXuz <silver-xuz@mail.ru>
2022-08-07 23:26:58 +0300

    Программа, которая покажет количество чётных чисел в массиве

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,13 +4,13 @@
 [345, 897, 568, 234] -> 2 */
 
 int n = 20;
-int[] array_first = new int[n];
+int[] array = new int[n];
 int count = 0;
 for (int i = 0; i < n; i++)
 {
-    array_first[i] = new Random().Next(100, 999);
-    if (array_first[i] % 2 == 0)
+    array[i] = new Random().Next(100, 999);
+    if (array[i] % 2 == 0)
         count++;
     
 }
-Console.WriteLine("[" + string.Join(", ", array_first) + "] четных -> " + count);
+Console.WriteLine("[" + string.Join(", ", array) + "] четных -> " + count);
